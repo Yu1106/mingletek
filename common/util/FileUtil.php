464a58e7377a4b2ec561aaef6d6b8e717abb1669
Yu1106 <fish731106@gmail.com>
2019-12-27 16:59:33 +0800

    feat: mingletek
    因應需求做調整：
     正式版step1 step2 測試
    修改程式

--- common/util/FileUtil.php
+++ common/util/FileUtil.php
@@ -6,7 +6,7 @@ use Exception;
 
 class FileUtil
 {
-	const IMG = '/var/www/mingletek/storage/upload/';
+	const IMG = '/home/mingletek/users/';
 	const CSV = '/var/www/mingletek/storage/download/';
 
 	const UPLOAD_DRESS = '/upload_dress';
