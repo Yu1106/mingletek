a94739c052c4131146a15e9be4a937aebc9fe5d9
Yu1106 <fish731106@gmail.com>
2019-12-30 17:22:57 +0800

    feat: mingletek
    因應需求做調整：
     api_post測試
    修改程式

--- resource/js/script.js
+++ resource/js/script.js
@@ -1,160 +1,328 @@
-$(function(){
+$(function () {
 
-	var timer
+    var timer
 
-	$(window).scroll(function() {
-		
-		window.clearTimeout(timer)
-		timer = window.setTimeout(function(){
+    $(window).scroll(function () {
 
-			var scrollTop = $(window).scrollTop();
+        window.clearTimeout(timer)
+        timer = window.setTimeout(function () {
 
-			//goTop
-			if ((scrollTop >= 500)){
-				$("header.fixed").addClass("is_scroll");
-				$("#goTop").stop().fadeIn();
-			}else{
-				$("header.fixed").removeClass("is_scroll");
-				$("#goTop").fadeOut();
-			}
+            var scrollTop = $(window).scrollTop();
 
-		},100)
+            //goTop
+            if ((scrollTop >= 500)) {
+                $("header.fixed").addClass("is_scroll");
+                $("#goTop").stop().fadeIn();
+            } else {
+                $("header.fixed").removeClass("is_scroll");
+                $("#goTop").fadeOut();
+            }
 
-	});
+        }, 100)
 
-	
-	//Scroll To Top
-	$("#goTop").click(function goTop(){
-		$("html,body").animate({scrollTop:0},800);
-	});
+    });
 
 
-	if($('#settingsForm').length > 0){
-		$('#settingsForm').validationEngine('attach', {
-			promptPosition: 'inline',
-			addPromptClass: 'errorMsg',
-			scroll: false,
-			maxErrorsPerField: 1,
-			onValidationComplete: function(form, status){
-				if(!status){
-					$.fancybox.open({
-						src: '#formErrorMsg',
-						afterClose : function() {
-							$('#formErrorMsg').empty();
-						}
-					});
-				}
-			}
-		});
-	}
+    //Scroll To Top
+    $("#goTop").click(function goTop() {
+        $("html,body").animate({scrollTop: 0}, 800);
+    });
 
 
-	$('.itemThumbnail').click(function(){
-		$('.itemThumbnail').removeClass('current');
-		$(this).addClass('current');
-	});
+    if ($('#settingsForm').length > 0) {
+        $('#settingsForm').validationEngine('attach', {
+            promptPosition: 'inline',
+            addPromptClass: 'errorMsg',
+            scroll: false,
+            maxErrorsPerField: 1,
+            onValidationComplete: function (form, status) {
+                if (!status) {
+                    $.fancybox.open({
+                        src: '#formErrorMsg',
+                        afterClose: function () {
+                            $('#formErrorMsg').empty();
+                        }
+                    });
+                }
+            }
+        });
+    }
 
 
-	$("#fileupload").change(function(){
-		// $("#previewBox").html(""); // 清除預覽
-		$('.sectionPreview').fadeIn();
-		readURL(this,false);
-	});
+    $('.itemThumbnail').click(function () {
+        $('.itemThumbnail').removeClass('current');
+        $(this).addClass('current');
+    });
 
 
-	$(".tabTrigger").click(function(){
+    $("#fileupload").change(function () {
+        $("#previewBox").html(""); // 清除預覽
+        $('.sectionPreview').fadeIn();
+        readURL(this, false);
+    });
+
+
+    $(".tabTrigger").click(function () {
         $(".tabs.active").toggleClass("active");
         $(this).addClass("active");
 
         var tabId = $(this).attr("data-tab");
         $(".tabContent.active").toggleClass("active");
-        $("#"+ tabId).addClass("active");
+        $("#" + tabId).addClass("active");
     });
 
 
-	if ($('.swiper-container').length > 0){
-		var galleryThumbs = new Swiper('.gallery-thumbs', {
-			spaceBetween: 10,
-			slidesPerView: 'auto',
-			freeMode: true,
-			watchSlidesVisibility: true,
-			watchSlidesProgress: true,
-		});
-		var galleryTop = new Swiper('.gallery-top', {
-			thumbs: {
-				swiper: galleryThumbs
-			}
-		});
+    if ($('.swiper-container').length > 0) {
+        var galleryThumbs = new Swiper('.gallery-thumbs', {
+            spaceBetween: 10,
+            slidesPerView: 'auto',
+            freeMode: true,
+            watchSlidesVisibility: true,
+            watchSlidesProgress: true,
+        });
+        var galleryTop = new Swiper('.gallery-top', {
+            thumbs: {
+                swiper: galleryThumbs
+            }
+        });
 
-		$('body').on("click", '.swiper-slide .icon-delete', function () {
-		    var i = $('.gallery-thumbs .swiper-slide').index($(this).closest(".swiper-slide"));
-			galleryThumbs.removeSlide(i);
-			galleryTop.removeSlide(i);
-		});
+        $('body').on("click", '.swiper-slide .icon-delete', function () {
+            var i = $('.gallery-thumbs .swiper-slide').index($(this).closest(".swiper-slide"));
+            galleryThumbs.removeSlide(i);
+            galleryTop.removeSlide(i);
+        });
 
-		$("#swiperupload").change(function(){
-			readURL(this,true);
-		});
-	}
+        $("#swiperupload").change(function () {
+            readURL(this, true);
+        });
+    }
 
-	if ($('#editForm').length > 0){
-		$('#editForm').validationEngine('attach', {
-			promptPosition: 'inline',
-			addPromptClass: 'errorMsg',
-			scroll: false,
-			maxErrorsPerField: 1,
-			onValidationComplete: function(form, status){
-				if(!status){
-					$.fancybox.open({
-						src: '#formErrorMsg',
-						afterClose : function() {
-							$('#formErrorMsg').empty();
-						}
-					});
-				}
-			}
-		});
-	}
+    if ($('#editForm').length > 0) {
+        $('#editForm').validationEngine('attach', {
+            promptPosition: 'inline',
+            addPromptClass: 'errorMsg',
+            scroll: false,
+            maxErrorsPerField: 1,
+            onValidationComplete: function (form, status) {
+                if (!status) {
+                    $.fancybox.open({
+                        src: '#formErrorMsg',
+                        afterClose: function () {
+                            $('#formErrorMsg').empty();
+                        }
+                    });
+                }
+            }
+        });
+    }
 
 
-	function readURL(input,swiperUpload){
-		if (input.files && input.files.length >= 0) {
-			for(var i = 0; i < input.files.length; i ++){
-				var reader = new FileReader();
-				reader.onload = function (e) {
-					if(swiperUpload) {
-						galleryThumbs.appendSlide('<div class="swiper-slide" style="background-image: url(' + e.target.result + ');"><i class="icon-delete"></i></div>');
-						galleryTop.appendSlide('<div class="swiper-slide" style="background-image: url(' + e.target.result + ');"></div>');
-					}else{
-						var img = $("<img class='previewImg'>").attr('src', e.target.result);
-						// console.log(e.target.result);
-						var thumbnail = $('<div class="previewThumbnail"><a class="icon-x-square" href="javascript:;"></a></div>').append(img);
-						$("#previewBox").prepend(thumbnail);
-					}
-				}
-				reader.readAsDataURL(input.files[i]);
-			}
-		}
-	}
-	
+    function readURL(input, swiperUpload) {
+        if (input.files && input.files.length >= 0) {
+            for (var i = 0; i < input.files.length; i++) {
+                var reader = new FileReader();
+                reader.fileName = input.files[i].name;
+                fileFormData.setValidate(input.files[i].name);
+                reader.onload = function (e) {
+                    if (swiperUpload) {
+                        galleryThumbs.appendSlide('<div class="swiper-slide" style="background-image: url(' + e.target.result + ');"><i class="icon-delete"></i></div>');
+                        galleryTop.appendSlide('<div class="swiper-slide" style="background-image: url(' + e.target.result + ');"></div>');
+                    } else {
+                        var img = $("<img class='previewImg'>").attr('src', e.target.result);
+                        var thumbnail = $('<div class="previewThumbnail"><a class="icon-x-square" href="javascript:void(0);" data-img="' + e.target.fileName + '" onclick="fileFormData.empty(this);"></a></div>').append(img);
+                        $("#previewBox").prepend(thumbnail);
+                    }
+                }
+                reader.readAsDataURL(input.files[i]);
+            }
+        }
+    }
 });
+
+var fileFormData = function () {
+
+    var form_data; //建構new FormData()
+    var status = true;
+    var msg = '';
+    var validateData = [];
+    var startProcessData = [];
+
+    var showErrorAlert = function () {
+        $("#alertMsgBox").html(msg);
+        showAlertMsg();
+    };
+    var reset = function () {
+        status = true;
+        msg = '';
+    };
+    var setFile = function () {
+        form_data = new FormData(); //建構new FormData()
+        var file_data = $("#fileupload").prop('files');  //取得上傳檔案屬性
+        if (file_data.length == 0 || validateData.length == 0)
+            return false;
+        for (var i = 0; i < file_data.length; i++) {
+            if ($.inArray(file_data[i].name, validateData) >= 0) {
+                form_data.append('file[]', file_data[i]);
+            }
+        }
+        form_data.append('action', '');
+        form_data.append('sub', '');
+        return true;
+    };
+    var setStartProcessData = function () {
+        $.ajax({
+            url: 'api_post.php',
+            data: {'action': 'startProcessData'},  //data只能指定單一物件
+            type: 'post',
+            async: false,
+            dataType: 'json',
+            success: function (data) {
+                startProcessData = data;
+            }
+        });
+    };
+    return {
+        validate: function (sub = false) {
+
+            if (!setFile())
+                return false;
+            form_data.set('action', 'validate');
+            if (sub) form_data.set('sub', 'sub');
+            reset();
+            showLoading();
+
+            $.ajax({
+                url: 'upload.php',
+                cache: false,
+                contentType: false,
+                processData: false,
+                data: form_data,  //data只能指定單一物件
+                type: 'post',
+                async: false,
+                dataType: 'json',
+                success: function (data) {
+                    $.each(data, function (k, v) {
+                        if (v.status == 0) {
+                            status = false;
+                            msg += v.name + ":" + v.msg + "<br>";
+                            $.each($(".icon-x-square"), function (k2, v2) {
+                                if ($(v2).attr("data-img") == v.name)
+                                    fileFormData.empty(v2);
+                            });
+                        } else {
+                            if ($.inArray(v.name, validateData) < 0) {
+                                validateData.push(v.name);
+                            }
+                        }
+                    });
+                    if (!status) {
+                        clearLoading();
+                        showErrorAlert();
+                    } else {
+                        fileFormData.upload(sub);
+                    }
+                    return false;
+                }
+            });
+            clearLoading();
+        },
+        upload: function (sub = false) {
+
+            form_data.set('action', 'upload');
+            if (sub) form_data.set('sub', 'sub');
+
+            $.ajax({
+                url: 'upload.php',
+                cache: false,
+                contentType: false,
+                processData: false,
+                data: form_data,  //data只能指定單一物件
+                type: 'post',
+                async: false,
+                dataType: 'json',
+                success: function (data) {
+                    $.each(data, function (k, v) {
+                        if (v.status == 0) {
+                            status = false;
+                            msg += v.name + ":" + v.msg + "<br>";
+                        }
+                    });
+                    if (!status) {
+                        clearLoading();
+                        showErrorAlert();
+                    } else {
+                        // $("#uploadMajor").submit();
+                    }
+                }
+            });
+        },
+        empty: function (e) {
+            var img = $(e).attr('data-img');
+            if (validateData.length > 0) {
+                validateData = validateData.filter(function (element) {
+                    return element != img;
+                });
+            }
+            var el = $(e).parent(".previewThumbnail");
+            el.remove();
+        },
+        setValidate: function (e) {
+            validateData.push(e);
+        },
+        startProcess: function () {
+            setStartProcessData();
+            var data = [startProcessData];
+            $.ajax({
+                type: 'POST',
+                url: '/longtask',
+                data: JSON.stringify(data),
+                contentType: "application/json",
+                dataType: 'json',
+                success: function (data, status, request) {
+                    var statusUrl = request.getResponseHeader('Location');
+                    fileFormData.updateProgress(statusUrl);
+                },
+                error: function () {
+                    alert('Unexpected error');
+                }
+            });
+        },
+        updateProgress: function (statusUrl) {
+            // send GET request to status URL
+            console.log(statusUrl);
+            $.getJSON(statusUrl, function (data) {
+                var percent = parseInt(data['current'] * 100 / data['total']);
+                console.log(percent);
+                // update UI
+                if (data['state'] != 'PENDING' && data['state'] != 'PROGRESS') {
+                    console.log(data);
+                } else {
+                    // rerun in 2 seconds
+                    setTimeout(function () {
+                        fileFormData.updateProgress(statusUrl);
+                    }, 2000);
+                }
+            });
+        }
+    }
+}();
 
 //hide loading spinner
 function clearLoading() {
-	$('.loading').fadeOut();
+    $('.loading').fadeOut();
 }
 
 //show loading spinner
 function showLoading() {
-	$('.loading').fadeIn();
+    $('.loading').fadeIn();
 }
 
 //show alertMsgBox
 function showAlertMsg() {
-	$.fancybox.open({
-		src: '#alertMsgBox',
-		afterClose : function() {
-			$('#alertMsgBox').empty();
-		}
-	});
+    $.fancybox.open({
+        src: '#alertMsgBox',
+        afterClose: function () {
+            $('#alertMsgBox').empty();
+        }
+    });
 }
