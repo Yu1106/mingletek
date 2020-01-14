d8006cf657ce59282b4a94ba60831e282f52fa30
Yu1106 <fish731106@gmail.com>
2020-01-14 18:07:52 +0800

    feat: mingletek
    因應需求做調整：
     step4製做
    修改程式

--- common/view/Asset.php
+++ common/view/Asset.php
@@ -10,13 +10,11 @@ class Asset
 <link rel="stylesheet" type="text/css" href="resource/css/form.css">
 <link rel="stylesheet" type="text/css" href="resource/css/jquery.fancybox.min.css">
 EOF;
-
 	public static $indexJs = <<<EOF
 <script type="text/javascript" src="resource/js/jquery-3.3.1.min.js"></script>
 <script type="text/javascript" src="resource/js/jquery.fancybox.min.js"></script>
 <script type="text/javascript" src="resource/js/script.js"></script>
 EOF;
-
 	public static $stepCss = <<<EOF
 <link rel="stylesheet" type="text/css" href="resource/css/reset.css">
 <link rel="stylesheet" type="text/css" href="resource/css/style.css">
@@ -24,7 +22,6 @@ EOF;
 <link rel="stylesheet" type="text/css" href="resource/css/jquery.fancybox.min.css">
 <link rel="stylesheet" type="text/css" href="resource/css/validationEngine.jquery.css">
 EOF;
-
 	public static $stepJs = <<<EOF
 <script type="text/javascript" src="resource/js/jquery-3.3.1.min.js"></script>
 <script type="text/javascript" src="resource/js/jquery.fancybox.min.js"></script>
@@ -32,7 +29,6 @@ EOF;
 <script type="text/javascript" src="resource/js/jquery.validationEngine.js"></script>
 <script type="text/javascript" src="resource/js/script.js"></script>
 EOF;
-
 	public static $step4Css = <<<EOF
 <link rel="stylesheet" type="text/css" href="resource/css/reset.css">
 <link rel="stylesheet" type="text/css" href="resource/css/style.css">
@@ -41,7 +37,6 @@ EOF;
 <link rel="stylesheet" type="text/css" href="resource/css/validationEngine.jquery.css">
 <link rel="stylesheet" type="text/css" href="resource/css/swiper.min.css">
 EOF;
-
 	static public $step4Js = <<<EOF
 <script type="text/javascript" src="resource/js/jquery-3.3.1.min.js"></script>
 <script type="text/javascript" src="resource/js/jquery.fancybox.min.js"></script>
