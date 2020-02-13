425947eac21203ae55ed0bd395b96e17c0acc9bd
Yu1106 <fish731106@gmail.com>
2020-02-13 11:28:20 +0800

    feat: mingletek
    因應需求做調整：
     step4製做
    修改程式

--- download.php
+++ download.php
@@ -298,10 +298,10 @@ if (is_array($exportFileLog)) {
 			$log = new LogUtil("export-" . date("Ymd"));
 			$log->error('exportCvs failed' . $e);
 		}
-		foreach ($array as $val) {
-			if (is_file($val))
-				unlink($val);
-		}
+//		foreach ($array as $val) {
+//			if (is_file($val))
+//				unlink($val);
+//		}
 		die;
 	} else if (count($exportFileLog) == 1) {
 		$fileName = $exportFileLog[0]['file_name'];
