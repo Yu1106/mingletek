65b2ffe7e291102b52cc8a65736007c7a58737ea
Yu1106 <fish731106@gmail.com>
2020-02-18 11:42:05 +0800

    feat: mingletek
    因應需求做調整：
     正是環境調整
    修改程式

--- resource/js/script.js
+++ resource/js/script.js
@@ -69,7 +69,7 @@ $(function () {
     });
 
 
-    $("#fileupload").change(function () {
+    $("#fileupload").on('change', function () {
         // $("#previewBox").html(""); // 清除預覽
         $('.sectionPreview').fadeIn();
         readURL(this, false);
