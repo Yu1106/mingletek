6d2a8665168f0d143571c0d64cf46cfb6cab3fef
Yu1106 <fish731106@gmail.com>
2020-03-03 16:00:04 +0800

    feat: mingletek
    因應需求做調整：
     正是環境調整
    修改程式

--- config/config.php
+++ config/config.php
@@ -5,8 +5,8 @@
 //define('DB_PASSWORD', '!QAZ2wsx');
 //define('DB_DATABASE', 'mingletek');
 // 測試主機
-define('DB_HOSTNAME', '219.91.63.48');
-define('DB_USERNAME', 'root');
+define('DB_HOSTNAME', 'clothes-test-1.c76r8y3b92kg.ap-northeast-1.rds.amazonaws.com');
+define('DB_USERNAME', 'admin');
 define('DB_PASSWORD', '!QAZ2wsx');
 define('DB_DATABASE', 'mingletek');
 
